ce673bfd5ab2e2012e5f7cb7d6e81afb1537a7db
hassoubeat <hassoubeat.work@gmail.com>
2020-09-09 17:57:32 +0900

    Update ドメイン変更に伴う修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,6 @@ Rails.application.configure do
   config.session_store :redis_store, servers: 'redis://localhost:6379/0', expire_in: 7.days
 
   # アプリケーションのデフォルトルートの設定
-  host = 'links.com:9090'
+  host = 'localhost:9090'
   Rails.application.routes.default_url_options[:host] = host
 end
